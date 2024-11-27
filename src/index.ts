518713888a028eccbe71da26a3c50a2710584438
aryansingh98875 <aryansingh98875@gmail.com>
2024-11-27 19:57:39 +0530

    checking deployment

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import express from "express";
 
 const app =express();
+// const db = new PrismaClient();
+
 
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello World');
 });
+
+// app.get('/users', async (req:Request, res:Response) => {
+//     const users = await db.user.findMany();
+//     res.json(users);
+// });
 
 
 app.listen(3000,()=>{
