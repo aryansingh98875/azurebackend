9d4910f4726d97aec76c1203e6df565336244d0b
aryansingh98875 <aryansingh98875@gmail.com>
2024-11-27 21:09:46 +0530

    started new

--- index.ts
+++ index.ts
@@ -1,19 +1,12 @@
-import { PrismaClient } from "@prisma/client";
 import express from 'express'
-const app =express();
-// const db = new PrismaClient();
 
+const app=express()
 
-app.get('/', (req, res) => {
-    res.json('Hello World');
-});
-
-// app.get('/users', async (req:Request, res:Response) => {
-//     const users = await db.user.findMany();
-//     res.json(users);
-// });
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
 
 
 app.listen(3000,()=>{
-    console.log("Listening on port 3000");
+    console.log('Server is running on port 3000')
 })
